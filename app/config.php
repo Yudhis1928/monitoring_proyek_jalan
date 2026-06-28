314aff53b5aa0185b4f0bdaa88fb8c4f199bf51b
Rex1928 <yudhistirataufik1928@gmail.com>
2026-06-29 01:11:27 +0700

    Connect DB

--- app/config.php
+++ app/config.php
@@ -1,14 +1,22 @@
 <?php
-if (!defined('DB_HOST')) {
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'monitoring_proyek_scrum');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('APP_NAME', 'Manajemen Infrastruktur Publik');
-    define('APP_TAGLINE', 'Jalan, Jembatan, dan Fasilitas Umum');
-    define('BASE_URL', '/monitoring_proyek_jalan'); 
-    define('UPLOAD_DIR', __DIR__ . '/../uploads');
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
+$host = 'bsfqntekdjqnvdoah3qc-mysql.services.clever-cloud.com'; 
+$db   = 'bsfqntekdjqnvdoah3qc';                                 
+$user = 'uw51gprlhmkkw8r8';                                 
+$pass = 'LUEGjpHB8XoVOkhvi9EH';                            
+$port = '3306';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     // Koneksi berhasil!
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+?>
